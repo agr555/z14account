ded7c42684f0eb102fe3b1daf34a16727302daf6
Your Name <agr555@mail.ru>
2023-11-17 18:16:22 +0100

    var-2 final

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,28 @@
 $(() => {
     // localStorage.clear();
-    let user = [];
-    let clients = [];
-    let userArray = [];
-
-    let inputFullName = $('#form-fullName');
-    let inputUsername = $('#form-username');
-    let inputFormEmail = $('#form-email');
-    let inputFormPassword = $('#form-password');
-    let inputFormRepeatPswd = $('#form-repeat-pswd');
-    let checkbox =  $('#form_choice');
-    let checkbox1= document.getElementById('form_choice');
-
-    /*   checkbox.addEventListener('change', (event) => {
-           if (event.currentTarget.checked) {
-               console.log("Согласен");
-           } else {
-               console.log("Не согласен");
-
-           }
-       });*/
+    const CLIENTS_LS_KEY = 'clients';
+    const elements = {
+        inputs: {
+            fullName: $('#form-fullName'), //!How!//    elements.inputs.fullName
+            fullNameText: $('#fullNameText'), //!How!//    elements.inputs.fullName
+            emailText: $('#emailText'), //!How!//    elements.inputs.fullName
+            repeatPswdText: $('#repeatPswdText'), //!How!//    elements.inputs.fullName
+            username: $('#form-username'),
+            formEmail: $('#form-email'),
+            formPassword: $('#form-password'),
+            formRepeatPswd: $('#form-repeat-pswd'),
+            checkAgree: $('#form_choice'),
+            //  checkbox1 : document.getElementById('form_choice'),
+            textAccountTitle: $('#account-title'),
+            textFormSign : $('#form-sign-btn'),
+            userHaveAccount : $('#userHaveAccount'),
+            linkText : $('#link-text'),
+            userAgree : $('#UserAgree'),
+            accountText : $('.account-text'),
+            formPlaceholderText : $('.form-placeholder-text')
+        },
+    };
+    let checkbox1 = document.getElementById('form_choice');
     const signButton = document.getElementById('form-sign-btn');
     const signLink = document.getElementById('userHaveAccount');
 
@@ -28,108 +31,105 @@ $(() => {
     signLink.onclick = goToLoginPage;
 
     function onErrorField(name) {
-        hasError = true;
         name.css('border-color', 'red');
         name.css('color', 'red');
         name.next().next().show();
     }
+
     function onErrorLogin(name) {
-        hasError = true;
         name.css('border-color', 'red');
         name.css('color', 'red');
         name.next().next().next().show();
     }
+
     function onEmptyField(name) {
-        hasError = true;
         name.css('border-color', 'red');
         name.next().show();
     }
-        function onUnChecked(name) {
-            hasError = true;
-           // name.css('color', 'red');
-            name.next().next().show();
-        }
-    function onRegister() {
-        //валидация
-        let hasError = false;
-        $('.error-input').hide();
-        $('.form-input').css('border-color', '#C6C6C4');
-        $('.form-input').css('color', '#323232' );
 
+    function onUnChecked(name) {
+        name.next().next().show();
+    }
+
+    function onRegister() {
+        $('.error-input').hide();
+        $('.form-input').css('border-color', '#C6C6C4').css('color', '#323232');
+        // $('.form-input').css('color', '#323232');
 
         //Full Name может содержать только буквы и пробел
-        if (!inputFullName.val()) {
-            onEmptyField(inputFullName);
+        if (!elements.inputs.fullName.val()) {
+            onEmptyField(elements.inputs.fullName);
             return;
         }
-        if (!inputFullName.val().match(/^[a-zA-ZА-Яа-яЁё+\s]*$/)) {
-            onErrorField(inputFullName);
+        if (!elements.inputs.fullName.val().match(/^[a-zA-ZА-Яа-яЁё+\s]*$/)) {
+            onErrorField(elements.inputs.fullName);
             return;
         }
         //Your username - может содержать только буквы, цифры, символ подчеркивания и тире
-        if (!inputUsername.val()) {
-            onEmptyField(inputUsername);
+        if (!elements.inputs.username.val()) {
+            onEmptyField(elements.inputs.username);
             return;
         }
-        // if (!inputUsername.val().match(/^[a-zA-ZА-Яа-яЁё\-_]+\s*$/)) {
-        if (!inputUsername.val().match(/^[А-Яа-яЁё\w-]+\s*$/)) {
-            onErrorField(inputUsername);
+        if (!elements.inputs.username.val().match(/^[А-Яа-яЁё\w-]+\s*$/)) {
+            onErrorField(elements.inputs.username);
             return;
         }
         //4. Реализовать проверку введенного E-mail на корректность
-        if (!inputFormEmail.val()) {
-            onEmptyField(inputFormEmail);
+        if (!elements.inputs.formEmail.val()) {
+            onEmptyField(elements.inputs.formEmail);
             return;
         }
-        if (!inputFormEmail.val().match(/^(?=.{1,30}$)[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
-            onErrorField(inputFormEmail);
+        if (!elements.inputs.formEmail.val().match(/^(?=.{1,30}$)[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            onErrorField(elements.inputs.formEmail);
             return;
         }
-        //5. Поле пароля должно содержать минимум 8 символов, среди которых есть:
-        //     хотя бы 1:  буква в верхнем регистре, цифра, спецсимвол
+        //5. Поле пароля : мин 8 символов, среди которых есть:
+        //     хотя бы 1:  буква в верх. регистре, цифра, спецсимвол
         //6. Password и Repeat Password должны совпадать
 
-        if (!inputFormPassword.val()) {
-            onEmptyField(inputFormPassword);
+        if (!elements.inputs.formPassword.val()) {
+            onEmptyField(elements.inputs.formPassword);
             return;
         }
-        if (!inputFormPassword.val().match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/)) {
-            onErrorField(inputFormPassword);
+        if (!elements.inputs.formPassword.val().match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/)) {
+            onErrorField(elements.inputs.formPassword);
             return;
         }
-        if (!inputFormRepeatPswd.val()) {
-            onEmptyField(inputFormRepeatPswd);
+        if (!elements.inputs.formRepeatPswd.val()) {
+            onEmptyField(elements.inputs.formRepeatPswd);
             return;
         }
 
-        if (inputFormRepeatPswd.val() != inputFormPassword.val()) {
-            onErrorField(inputFormRepeatPswd);
+        if (elements.inputs.formRepeatPswd.val() !== elements.inputs.formPassword.val()) {
+            onErrorField(elements.inputs.formRepeatPswd);
             return;
         }
 //7. Пользователь должен согласиться с условиями
         if (!checkbox1.checked) {
-            onUnChecked(checkbox);
-            console.log(checkbox.value);
-            console.log(checkbox);
+            onUnChecked(elements.inputs.checkAgree);
+            console.log(elements.inputs.checkAgree.value);
+            console.log(elements.inputs.checkAgree);
             return;
         }
-        console.log(checkbox.value);
-        let user = {
-            fullName: document.querySelector('#userFullName :nth-child(2)').value,
-            userName: document.querySelector('#userName :nth-child(2)').value,
-            pswd: document.querySelector('#userPswd :nth-child(2)').value,
-            agreeName: document.querySelector('#UserAgree :nth-child(2)'),
-        };
+        console.log(elements.inputs.checkAgree.value);
 
-        let clients = JSON.parse(localStorage.getItem('clients')) || [];
-        clients.push(user);
-        userArray.push(clients);
-        localStorage.setItem('clients', JSON.stringify(clients));
+        onWriteLocalStorage();
+        function onWriteLocalStorage() {
+            let user = {
+                fullName: document.querySelector('#userFullName :nth-child(2)').value,
+                userName: document.querySelector('#userName :nth-child(2)').value,
+                pswd: document.querySelector('#userPswd :nth-child(2)').value,
+                agreeName: document.querySelector('#UserAgree :nth-child(2)'),
+            };
 
+            let clients = JSON.parse(localStorage.getItem(CLIENTS_LS_KEY)) || [];
+            clients.push(user);
+            // userArray.push(clients);
+            localStorage.setItem(CLIENTS_LS_KEY, JSON.stringify(clients));
+        }
 
         let popupShow = document.getElementById('popup-show');
         let popupBtn = document.getElementById('popup-btn');
-        // confirm('На вашу почту выслана ссылка, перейдите по ней, чтобы завершить регистрацию.');
         popupShow.classList.add('opened');
         popupBtn.addEventListener('click', function () {
             popupShow.classList.remove('opened');
@@ -138,106 +138,102 @@ $(() => {
     }
 
     function goToLoginPage() {
-        inputFullName.value = '';
-        inputUsername.value = '';
-        inputFormEmail.value = '';
-        inputFormPassword.value = '';
-        inputFormRepeatPswd.value = '';
+        elements.inputs.fullName.value = '';
+        elements.inputs.username.value = '';
+        elements.inputs.formEmail.value = '';
+        elements.inputs.formPassword.value = '';
+        elements.inputs.formRepeatPswd.value = '';
 
-        let TextAccountTitle = document.getElementById('account-title');
-        TextAccountTitle.innerHTML = 'Log in to the system';
-        let TextFormSign = $('#form-sign-btn');
-        TextFormSign.html('Sign In');
-        let userHaveAccount = $('#userHaveAccount');
-        let linkText = $('#link-text');
 
-        linkText.html("Registration");
 
-        let userFullName = $('#userFullName');
+        elements.inputs.textAccountTitle[0].innerHTML = 'Log in to the system';
+        elements.inputs.textFormSign.html('Sign In');
+        elements.inputs.linkText.html("Registration");
+
+        elements.inputs.fullName.remove();
+        elements.inputs.fullNameText.remove();
+        elements.inputs.formEmail.remove();
+        elements.inputs.emailText.remove();
+        elements.inputs.formRepeatPswd.remove();
+        elements.inputs.repeatPswdText.remove();
+
+         elements.inputs.userAgree.remove();
+       // elements.inputs.fullNamePlaceHolder.remove();
+    /*    let userFullName = $('#userFullName');
+        userFullName.remove();
         let userEmail = $('#userEmail');
         let userRepeatPswd = $('#userRepeatPswd');
-        let userAgree = $('#UserAgree');
-
-        userFullName.remove();
         userEmail.remove();
-        userRepeatPswd.remove();
-        userAgree.remove();
+        userRepeatPswd.remove();*/
+
         signButton.onclick = onLogin;
-        if (TextAccountTitle.innerHTML == 'Log in to the system') {
-            // signLink.onclick = location.reload();
+        if (elements.inputs.textAccountTitle[0].innerHTML === 'Log in to the system') {
             signLink.onclick = onRegister;
-            //window.onload;
             signLink.addEventListener('click', () => window.location.reload());
         }
     }
 
     function onLogin() {
         $('.error-input').hide();
-        $('.form-input').css('border-color', '#C6C6C4');
-        $('.form-input').css('color', '#323232' );
+        $('.form-input').css('border-color', '#C6C6C4').css('color', '#323232');
+        // $('.form-input').css('color', '#323232');
 
-        if (!inputUsername.val()) {
-            onEmptyField(inputUsername);
+        if (!elements.inputs.username.val()) {
+            onEmptyField(elements.inputs.username);
             return;
         }
-        if (!inputFormPassword.val()) {
-            onEmptyField(inputFormPassword);
+        if (!elements.inputs.formPassword.val()) {
+            onEmptyField(elements.inputs.formPassword);
             return;
         }
-
         let clients = JSON.parse(localStorage.getItem("clients")) || [];
         let userFind = '';
-        let pswdFind = 0;
-        for (let i = 0; i < clients.length; i++) {
-            if (clients[i].userName == inputUsername.val()) {
-                userFind = clients[i].fullName;
-                if (clients[i].pswd != inputFormPassword.val()) {
-                    console.log("Неверный пароль");
-                    onErrorLogin(inputFormPassword);
-                   } else {
-                    console.log("Добро пожаловать, " + clients[i].fullName + "!");
-                    // signButton.onAccount;
-                    onAccount(userFind);
+        // let pswdFind = 0;
+
+        try {
+            clients.forEach((item) => {
+                if (item.userName === elements.inputs.username.val()) {
+                    userFind = item.fullName;
+                    if (item.pswd !== elements.inputs.formPassword.val()) {
+                        console.log("Неверный пароль");
+                        onErrorLogin(elements.inputs.formPassword);
+                    } else {
+                        console.log("Добро пожаловать, " + item.fullName + "!");
+                        onAccount(userFind);
+                    }
+                    // break in try-catch;
                 }
-                break;
+            });
+        } catch (e) {
+                if (e.message !== 'catch Value found') {
+                    throw e;
+                }
             }
-
-            }
-        if (userFind == '') {
+        if (userFind === '') {
             console.log('Такой пользователь не зарегистрирован');
-            onErrorLogin(inputUsername);
+            onErrorLogin(elements.inputs.username);
         }
-
     }
-
     function onAccount(userFind) {
-        inputUsername.value = '';
-        inputFormPassword.value = '';
+        elements.inputs.formPlaceholderText.remove();
+        elements.inputs.username.value = '';
+        elements.inputs.formPassword.value = '';
         $('.error-input').hide();
-        $('.form-input').css('border-color', '#C6C6C4');
-        $('.form-input').css('color', '#323232' );
+        $('.form-input').css('border-color', '#C6C6C4').css('color', '#323232');
+        // $('.form-input').css('color', '#323232');
+        elements.inputs.textAccountTitle[0].innerHTML = 'Welcome, ' + userFind + '!';
+        elements.inputs.textFormSign.html('Exit');
+        elements.inputs.linkText.html("Registration");
 
+        elements.inputs.username.remove();
+        elements.inputs.formPassword.remove();
+        elements.inputs.linkText.remove();
+        elements.inputs.accountText.remove();
 
-        let TextAccountTitle = document.getElementById('account-title');
-        TextAccountTitle.innerHTML = 'Welcome, ' + userFind + '!';
-        let TextFormSign = $('#form-sign-btn');
-        TextFormSign.html('Exit');
-        let userHaveAccount = $('#userHaveAccount');
-        let accountText = $('.account-text');
-        let formPlaceholderText = $('.form-placeholder-text');
-        let linkText = $('#link-text');
-        linkText.html("Registration");
-
-        inputUsername.remove();
-        inputFormPassword.remove();
-        linkText.remove();
-        accountText.remove();
-        formPlaceholderText.remove();
         signButton.addEventListener('click', () => window.location.reload());
     }
-
 });
 $(document).on('submit', 'form', function (e) {
     e.preventDefault(); // или же return false
 });
-//Qwerty1@ss!
+//Qwerty123!
